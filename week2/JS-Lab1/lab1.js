fc3250d337fe868fb9245a6e7dd917edfada3f28
Kexin Sun <kexin.sun82@gmail.com>
2024-09-10 23:38:02 -0400

    Added week2 folder

---
+++ week2/JS-Lab1/lab1.js
@@ -0,0 +1,17 @@
+//alert("Hello World");
+
+var nameKexin = "Kexin Sun";
+
+var age = 24;
+
+var numberOfYear = 365;
+
+var ageTotal = age * numberOfYear;
+
+var text = "days old... more or less."
+
+//alert(ageTotal);
+
+var reads = nameKexin + " " + "is" + " " + ageTotal + " " + text;
+
+alert(reads);
